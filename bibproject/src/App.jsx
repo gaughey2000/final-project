47da2b8beea22564bb0ffe30efb533eb485bd554
Connor McGaughey <connormcgaughey@Connors-MacBook-Air.local>
2025-02-07 14:02:17 +0000

    3rd commit

--- bibproject/src/App.jsx
+++ bibproject/src/App.jsx
@@ -10,15 +10,21 @@ function App() {
 
   return (
     <>
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/procedures" element={<Procedures />} />
-        <Route path="/whyus" element={<Whyus />} />
-        <Route path="/voucher" element={<Voucher />} />
-        <Route path="/book" element={<Book />} />
-        <Route path="/contact" element={<Contact />} />
-      </Routes>
+      <div className="w-full h-full absolute
+      bg-gradient-to-b from-slate-500 to-white">
+        <header className="flex justify-between items-center
+        text-black py-6 px-8 md:px-32 bg-white drop-shadow-md">
+          <Navbar />
+        </header>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/procedures" element={<Procedures />} />
+          <Route path="/whyus" element={<Whyus />} />
+          <Route path="/voucher" element={<Voucher />} />
+          <Route path="/book" element={<Book />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+      </div>
     </>
   )
 }
--- bibproject/src/Components/Navbar.jsx
+++ bibproject/src/Components/Navbar.jsx
@@ -2,18 +2,18 @@ import { NavLink } from "react-router";
 
 function Navbar() {
     return (
-        <div className="w-screen flex flex-wrap items-center justify-between mx-auto bg-blue-400">
+        <div className="w-screen flex flex-wrap items-center justify-between mx-auto">
             <div className="flex items-center space-x-3 rtl:space-x-reverse">
-                <img src="https://www.bibclinic.co.uk/wp-content/uploads/2017/06/bib_clinic_logo_grey.png" alt="BiB Clinic Logo" className="h-30 p-2" />
+                <img src="https://www.bibclinic.co.uk/wp-content/uploads/2017/06/bib_clinic_logo_grey.png" alt="BiB Clinic Logo" className="w-30 hover:scale-110 transition-all duration-300" />
             </div>
             <div>
                 <ul className='flex flex-row'>
-                    <li className='p-2'><NavLink to="/">Home</NavLink></li>
-                    <li className='p-2'><NavLink to="/procedures">Procedures</NavLink></li>
-                    <li className='p-2'><NavLink to="/whyus">Why Us</NavLink></li>
-                    <li className='p-2'><NavLink to="/contact">Contact</NavLink></li>
-                    <li className='p-2'><NavLink to="/book">Book Now</NavLink></li>
-                    <li className='p-2'><NavLink to="/voucher">Gift Vouchers</NavLink></li>
+                    <li className='p-8'><NavLink to="/">Home</NavLink></li>
+                    <li className='p-8'><NavLink to="/procedures">Procedures</NavLink></li>
+                    <li className='p-8'><NavLink to="/whyus">Why Us</NavLink></li>
+                    <li className='p-8'><NavLink to="/contact">Contact</NavLink></li>
+                    <li className='p-8'><NavLink to="/book">Book Now</NavLink></li>
+                    <li className='p-8'><NavLink to="/voucher">Gift Vouchers</NavLink></li>
                 </ul>
             </div>
         </div>
