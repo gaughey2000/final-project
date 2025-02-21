769b6b3b03b729bdb0f15d5e0633256eb7c15300
Connor McGaughey <cmcgaughey2000@icloud.com>
2025-02-21 11:41:41 +0000

    7th commit

--- bibproject/src/Components/Contact.jsx
+++ bibproject/src/Components/Contact.jsx
@@ -1,7 +1,14 @@
 function Contact() {
     return (
         <div>
-            <h1>Contact</h1>
+            <a href="#" class="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow-sm hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
+                <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Get in touch</h5>
+                <p class="font-normal text-gray-700 dark:text-gray-400">
+                    Phone: 0123456789
+                    <br />Email: email@adress.com
+                    <br />Address: 1234 Street Name, City Name, Country Name
+                </p>
+            </a>
         </div>
     )
 }
--- bibproject/src/Components/Whyus.jsx
+++ bibproject/src/Components/Whyus.jsx
@@ -1,7 +1,10 @@
 function Whyus() {
     return (
         <div>
-            <h1>Why Us</h1>
+            <a href="#" class="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow-sm hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
+                <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Why choose us?</h5>
+                <p class="font-normal text-gray-700 dark:text-gray-400">Because we are by far the best.</p>
+            </a>
         </div>
     )
 }
