9ea9fe88f7f3254b742bfcabdf751d7c8de9d62d
Connor McGaughey <cmcgaughey2000@icloud.com>
2025-02-25 10:16:47 +0000

    8th commit

--- bibproject/src/Components/Navbar.jsx
+++ bibproject/src/Components/Navbar.jsx
@@ -8,12 +8,12 @@ function Navbar() {
             </div>
             <div>
                 <ul className='flex flex-row'>
-                    <li className='p-8'><NavLink to="/">Home</NavLink></li>
-                    <li className='p-8'><NavLink to="/procedures">Procedures</NavLink></li>
-                    <li className='p-8'><NavLink to="/whyus">Why Us</NavLink></li>
-                    <li className='p-8'><NavLink to="/contact">Contact</NavLink></li>
-                    <li className='p-8'><NavLink to="/book">Book Now</NavLink></li>
-                    <li className='p-8'><NavLink to="/voucher">Gift Vouchers</NavLink></li>
+                    <li className='p-8'><NavLink to="/">HOME</NavLink></li>
+                    <li className='p-8'><NavLink to="/procedures">PROCEDURES</NavLink></li>
+                    <li className='p-8'><NavLink to="/whyus">WHY US</NavLink></li>
+                    <li className='p-8'><NavLink to="/contact">CONTACT</NavLink></li>
+                    <li className='p-8'><NavLink to="/book">BOOK NOW</NavLink></li>
+                    <li className='p-8'><NavLink to="/voucher">GIFT VOUCHERS</NavLink></li>
                 </ul>
             </div>
         </div>
