7130401fcc0b8348c9c239bab6ebd9995c10aa66
Connor McGaughey <cmcgaughey2000@icloud.com>
2025-03-19 13:13:04 +0000

    14.1th commit

--- bibproject/src/Pages/NewHome.jsx
+++ bibproject/src/Pages/NewHome.jsx
@@ -4,9 +4,9 @@ function NewHome() {
     <>
         <div className="w-full h-full flex flex-col items-center justify-center">
             {/*top section*/}
-            <div className="w-full h-1/2 text-center">
+            <div className="w-full h-1/2 text-center relative">
                 <img src="https://placehold.co/1500x500" alt="profile picture" className="w-full h-full" />
-                <h1 className="text-4xl font-bold text-center text-black absolute">Hello and welcome to BiB Clinic</h1>
+                <h1 className="text-4xl font-bold text-center text-black absolute bottom-30 left-150">Hello and welcome to BiB Clinic</h1>
             </div>
             {/*main section*/}
             <div className="w-250 h-1/2 flex flex-col items-center justify-center bg-white border border-gray-200 rounded-lg shadow-sm dark:bg-gray-800 dark:border-gray-700 m-10" >
