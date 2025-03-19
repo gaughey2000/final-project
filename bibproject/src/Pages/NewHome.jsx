4d07ad1ec745b8053a3e623657c3740024d7dce7
Connor McGaughey <cmcgaughey2000@icloud.com>
2025-03-19 12:18:57 +0000

    14th commit

--- bibproject/src/Pages/NewHome.jsx
+++ bibproject/src/Pages/NewHome.jsx
@@ -1,11 +1,23 @@
 function NewHome() {
   return (
-    <div className="w-full h-full flex flex-col items-center justify-center">
-      <div className="w-full h-1/2 bg-white border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700 items-center">
-        <img src="https://placehold.co/1500x500" alt="profile picture" className="w-full h-1/2" />
-        <h1 className="text-4xl font-bold text-center text-black">Hello and welcome to BiB Clinic</h1>
-      </div>
-    </div>
+    
+    <>
+        <div className="w-full h-full flex flex-col items-center justify-center">
+            {/*top section*/}
+            <div className="w-full h-1/2 text-center">
+                <img src="https://placehold.co/1500x500" alt="profile picture" className="w-full h-full" />
+                <h1 className="text-4xl font-bold text-center text-black absolute">Hello and welcome to BiB Clinic</h1>
+            </div>
+            {/*main section*/}
+            <div className="w-250 h-1/2 flex flex-col items-center justify-center bg-white border border-gray-200 rounded-lg shadow-sm dark:bg-gray-800 dark:border-gray-700 m-10" >
+                <ul className="list-disc list-inside text-xl text-center text-black">
+                    <li>Some</li>
+                    <li>General</li>
+                    <li>Information</li>
+                </ul>
+            </div>
+        </div>
+    </>
   );
 }
 
