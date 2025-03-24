e3dad4c58b5b1c6a284e8c9ecef93c7e1a393fed
Connor McGaughey <cmcgaughey2000@icloud.com>
2025-03-24 14:43:03 +0000

    16th commit

--- bibproject/src/Pages/Home.jsx
+++ bibproject/src/Pages/Home.jsx
@@ -2,7 +2,10 @@ function Home() {
     return (
         <div className="grid grid-rows-3 grid-flow-col gap-4">
             <div className="row-span-3 ... max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow-sm dark:bg-gray-800 dark:border-gray-700 ml-25 mt-16">
-                <img src="https://placehold.co/400" alt="profile picture" />
+                <video width="320" height="240" controls>
+                    <source src="../media/intro.mp4" type="video/mp4" />
+                    Your browser does not support the video tag.
+                </video>
             </div>
             <div className="col-span-2 ... pr-10 mt-16 max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow-sm dark:bg-gray-800 dark:border-gray-700">
                 <h1 className="text-4xl font-bold text-center text-white">Hello and welcome to BiB Clinic</h1>
--- bibproject/src/Pages/NewHome.jsx
+++ bibproject/src/Pages/NewHome.jsx
@@ -1,16 +1,20 @@
+import video from "../media/intro.mp4";
+
 function NewHome() {
   return (
     
     <>
         <div className="w-full h-full flex flex-col items-center justify-center">
             {/*top section*/}
-            <div className="w-full h-1/2 text-center relative">
-                <img src="https://placehold.co/1500x500" alt="profile picture" className="w-full h-full" />
-                <h1 className="text-4xl font-bold text-center text-black absolute bottom-30 left-150">Hello and welcome to BiB Clinic</h1>
+            <div className="w-full h-1/2 felx relative top-1 left-1/8 ">
+                <video width="75%" height="auto" controls className="justify-center rounded-lg shadow-sm dark:bg-gray-800 dark:border-gray-700">
+                    <source src={video} type="video/mp4" />
+                    Your browser does not support the video tag.
+                </video>
             </div>
             {/*main section*/}
             <div className="w-250 h-1/2 flex flex-col items-center justify-center bg-tertiary rounded-lg shadow-sm dark:bg-gray-800 dark:border-gray-700 m-10" >
-                <ul className="list-disc list-inside text-xl text-center text-black">
+                <ul className="list-none list-inside text-xl text-center text-black">
                     <li>Some</li>
                     <li>General</li>
                     <li>Information</li>
