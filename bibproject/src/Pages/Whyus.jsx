4801f27bcafca23f6fa8320f1d5280343e9f9dcd
Connor McGaughey <cmcgaughey2000@icloud.com>
2025-04-12 11:22:38 +0100

    32

--- bibproject/src/Pages/Whyus.jsx
+++ bibproject/src/Pages/Whyus.jsx
@@ -1,6 +1,23 @@
 import headshot from "../media/head-shot.png"
 import { useState } from "react";
+
 function Whyus() {
+
+    const [isMenu1Open, setIsMenu1Open] = useState(false);
+    const handleMenu1Toggle = () => {
+        setIsMenu1Open(!isMenu1Open);
+    }
+
+    const [isMenu2Open, setIsMenu2Open] = useState(false);
+    const handleMenu2Toggle = () => {
+        setIsMenu2Open(!isMenu2Open);
+    }
+
+    const [isMenu3Open, setIsMenu3Open] = useState(false);
+    const handleMenu3Toggle = () => {
+        setIsMenu3Open(!isMenu3Open);
+    }
+
     return (
         <div className="">
             {/*Rachel*/}
@@ -8,8 +25,11 @@ function Whyus() {
                 <div classname="">
                     <h1 className="text-xl font-bold text-gray-900 truncate">Meet Rachel</h1>
                     <img src={headshot} alt="Rachel" className="w-48 h-48 rounded-full" />
+                    {isMenu1Open ? <i onClick={handleMenu1Toggle} className='bx bxs-chevron-up' ></i> : <i onClick={handleMenu1Toggle} className='bx bxs-chevron-down' ></i>}
+                    
+                    <box-icon name='chevron-up' type='solid' ></box-icon>
                 </div>
-                <div className="">
+                <div className={isMenu1Open ? 'block' : 'hidden'}>
                     <p>
                         <br />
                         <strong>Best of the best</strong>
@@ -37,47 +57,61 @@ function Whyus() {
             {/*Why Us*/}
             <div className="mt-16 my-1 bg-white border border-gray-200 rounded-lg shadow-sm sm:p-8 items-center">
                 <h1 className="font-bold">Why choose us?</h1>
-                <p className="">
-                    <strong>Begginings</strong>
+                {isMenu2Open ? <i onClick={handleMenu2Toggle} className='bx bxs-chevron-up' ></i> : <i onClick={handleMenu2Toggle} className='bx bxs-chevron-down' ></i>}
+                <div className={isMenu2Open ? 'block' : 'hidden'}>
+                    <strong><p>Begginings</p></strong>
                     <br/>
-                    All the aesthetic treatments are carried out by an experienced professional, registered nurse 
-                    and independent prescriber Rachel McGaughey, who founded BiB.
+                    <p>
+                        All the aesthetic treatments are carried out by an experienced professional, registered nurse 
+                        and independent prescriber Rachel McGaughey, who founded BiB.
+                    </p>
                     <br />
                     <br />
-                    <strong>Individual</strong>
+                    <strong><p>Individual</p></strong>
                     <br/>
-                    At BiB Aesthetics Clinic, our aim is to provide individually tailored treatments that deliver  
-                    natural looking results.
+                    <p>
+                        At BiB Aesthetics Clinic, our aim is to provide individually tailored treatments that deliver  
+                        natural looking results.
+                    </p>
                     <br />
                     <br />
-                    <strong>Beauty</strong>
+                    <strong><p>Beauty</p></strong>
                     <br/>
+                    <p>
                     Dedicated and commited to the highest ethical standards. We use the latest innovative treatments with high 
                     end products to ensure you look and feel beautiful
                     </p>
+                </div>
             </div>
 
             
             {/*Our Promise*/}
             <div className="mt-16 my-1 bg-white border border-gray-200 rounded-lg shadow-sm sm:p-8 items-center">
                 <h5 className="">Our Promise</h5>
-                <p className="">
-                    <strong>Confidence</strong>
+                {isMenu3Open ? <i onClick={handleMenu3Toggle} className='bx bxs-chevron-up' ></i> : <i onClick={handleMenu3Toggle} className='bx bxs-chevron-down' ></i>}
+                <div className={isMenu3Open ? 'block' : 'hidden'}>
+                    <strong><p>Confidence</p></strong>
                     <br/>
-                    Boost your confidence, appearance and wellbeing.
+                    <p>
+                        Boost your confidence, appearance and wellbeing.
+                    </p>
                     <br />
                     <br />
-                    <strong>Individual</strong>
+                    <strong><p>Individual</p></strong>
                     <br/>
-                    At BiB Aesthetics Clinic, our aim is to provide individually tailored treatments that deliver  
-                    natural looking results.
+                    <p>
+                        At BiB Aesthetics Clinic, our aim is to provide individually tailored treatments that deliver  
+                        natural looking results.
+                    </p>
                     <br />
                     <br />
-                    <strong>Beauty</strong>
+                    <strong><p>Beauty</p></strong>
                     <br/>
-                    Dedicated and commited to the highest ethical standards. We use the latest innovative treatments with high 
-                    end products to ensure you look and feel beautiful.
-                </p>
+                    <p>
+                        Dedicated and commited to the highest ethical standards. We use the latest innovative treatments with high 
+                        end products to ensure you look and feel beautiful.
+                    </p>    
+                </div>
             </div>
         </div>
     )
